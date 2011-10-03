169f1085183c147f7227c5fa6993339126d9dd6e
João Magalhães <joamag@gmail.com>
2011-10-04 00:20:12 +0100

    minor code refactor

--- mingus/mingus.cpp
+++ mingus/mingus.cpp
@@ -14,26 +14,26 @@ unsigned regs[NUM_REGS];
  * to be executed for testing.
  */
 unsigned program[] = {
-	0x1064,
-	0x11C8,
-	0x2201,
-	0x0000
+    0x1064,
+    0x11C8,
+    0x2201,
+    0x0000
 };
- 
+
 int pc = 0;
- 
+
 /* fetch the next word from the program */
 int fetch() {
   return program[pc++];
 }
- 
+
 /* instruction fields */
 int instrNum = 0;
 int reg1 = 0;
 int reg2 = 0;
 int reg3 = 0;
 int imm = 0;
- 
+
 /* decode a word */
 void decode(int instr) {
   instrNum = (instr & 0xF000) >> 12;
@@ -42,93 +42,92 @@ void decode(int instr) {
   reg3 = (instr & 0x000F);
   imm = (instr & 0x00FF);
 }
- 
+
 /* the VM runs until this flag becomes 0 */
 int running = 1;
 
 typedef struct State {
-	int running;
+    int running;
 
 } State_t;
- 
+
 /* evaluate the last decoded instruction */
 void eval() {
-	/* swtiches over the instruction number */
+    /* swtiches over the instruction number */
     switch(instrNum) {
-		/* in case it's the halt instruction */
+        /* in case it's the halt instruction */
         case 0:
             printf("halt\n");
 
-			/* unsets the runnig flag */
+            /* unsets the runnig flag */
             running = 0;
 
-			/* breaks the switch */
+            /* breaks the switch */
             break;
-        
-		/* in case it's the loadi instruction */
-		case 1:
+
+        /* in case it's the loadi instruction */
+        case 1:
             printf("loadi r%d #%d\n", reg1, imm);
 
-			/* sets the integer in the register */
+            /* sets the integer in the register */
             regs[reg1] = imm;
 
-			/* breaks the switch */
-			break;
-    
-		/* in case it's the add instruction */
-		case 2:
-			printf("add r%d r%d r%d\n", reg1, reg2, reg3);
+            /* breaks the switch */
+            break;
 
-			/* sums both registers and puts the result
-			in the third register */
-			regs[reg1] = regs[reg2] + regs[reg3];
-      
-			/* breaks the switch */
-			break;
+        /* in case it's the add instruction */
+        case 2:
+            printf("add r%d r%d r%d\n", reg1, reg2, reg3);
+
+            /* sums both registers and puts the result
+            in the third register */
+            regs[reg1] = regs[reg2] + regs[reg3];
+
+            /* breaks the switch */
+            break;
     }
 }
 
 void showRegs() {
-	/* allocates the index */
+    /* allocates the index */
     int index;
 
-	/* prints the initial line */
+    /* prints the initial line */
     printf("regs = ");
-  
-	/* iterates over all the registers */
+
+    /* iterates over all the registers */
     for(index = 0; index < NUM_REGS; index++) {
         printf("%04X ", regs[index]);
     }
 
-	/* prints the newline */
+    /* prints the newline */
     printf( "\n" );
 }
- 
+
 void run() {
-	/* iterates while the running flag is set */
+    /* iterates while the running flag is set */
     while(running) {
-		/* shows the registers */
+        /* shows the registers */
         showRegs();
 
-		/* fetches the next instruction */
+        /* fetches the next instruction */
         int instruction = fetch();
 
-		/* decodes the instruction */
+        /* decodes the instruction */
         decode(instruction);
 
-		/* evaluates the current state */
+        /* evaluates the current state */
         eval();
     }
-  
-	/* shows the registers */
-	showRegs();
-}
- 
-int main(int argc, const char * argv[]) {
-	/* runs the virtual machine */
-    run();
-  
-	/* returns with no error */
-	return 0;
+
+    /* shows the registers */
+    showRegs();
 }
 
+int main(int argc, const char * argv[]) {
+    /* runs the virtual machine */
+    run();
+
+    /* returns with no error */
+    return 0;
+}
