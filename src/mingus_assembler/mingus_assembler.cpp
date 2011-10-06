d4ccb073fcca11140f19df50f59512464ee4c513
João Magalhães <joamag@gmail.com>
2011-10-06 01:01:39 +0100

    minor code change

--- src/mingus_assembler/mingus_assembler.cpp
+++ src/mingus_assembler/mingus_assembler.cpp
@@ -1,7 +1,33 @@
+/*
+ Mingus Virtual Machine
+ Copyright (C) 2008 João Magalhães
+
+ This file is part of Mingus Virtual Machine.
+
+ Mingus Virtual Machine is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ Mingus Virtual Machine is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Mingus Virtual Machine. If not, see <http://www.gnu.org/licenses/>.
+
+ __author__    = João Magalhães <joamag@hive.pt>
+ __version__   = 1.0.0
+ __revision__  = $LastChangedRevision$
+ __date__      = $LastChangedDate$
+ __copyright__ = Copyright (c) 2008 João Magalhães
+ __license__   = GNU General Public License (GPL), Version 3
+*/
+
 #include "stdafx.h"
 
-int _tmain(int argc, _TCHAR* argv[])
-{
+int main(int argc, const char *argv[]) {
 	return 0;
 }
 
